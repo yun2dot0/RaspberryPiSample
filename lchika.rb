b2362f244b84c5b9a6e2ed3338f99b179692a981
yun2dot0 <yun2dot0@gmail.com>
2015-03-27 21:47:24 +0900

    offした後にsleepが入ってなかったので修正

--- lchika.rb
+++ lchika.rb
@@ -1,10 +1,11 @@
 require 'pi_piper'
 include PiPiper
 
-pin = :Pin.new(:pin => 25, :direction => :out)
+pin = ::Pin.new(:pin => 25, :direction => :out)
 
 loop do
 	pin.on
 	sleep 1
 	pin.off
+	sleep 1
 end
